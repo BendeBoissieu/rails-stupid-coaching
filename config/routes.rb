1ae285f4d40096523c0f855a2219c579b1bf5c5f
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-06-04 15:06:07 +0200

    first 2 pages ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
+
+
+  root to: 'pages#home'
+  end
